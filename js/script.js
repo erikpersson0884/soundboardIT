6b9aef3c1049d0235c25487fb78431497523af52
Erik <erikpersson0884@gmail.com>
2024-09-05 14:55:03 +0200

    restrucutred js

--- js/script.js
+++ js/script.js
@@ -1,86 +1,42 @@
-const sounds = [
-    {
-        name: "John Doe", 
-        soundFile: "sounds/1.wav"
-    },
-    {
-        name: "Jane Doe",
-        soundFile: "sounds/boom.wav"
-    },
-    {
-        name: "Hund",
-        soundFile: "sounds/dog.wav"
-    },
-    {
-        name: "laugh",
-        soundFile: "sounds/laugh.wav"
-    },
-    {
-        name: "retro",
-        soundFile: "sounds/retro.wav"
-    },
-    {
-        name: "oooh my god",
-        soundFile: "sounds/ohmygod.wav",    
-        image: "images/ohmygod.jpg"
-    }
-]
-
 const soundsDiv = document.getElementById('soundBoard');
 
-const numRows = Math.ceil(Math.sqrt(sounds.length)); // Calculate number of rows
-const numCols = Math.ceil(sounds.length / numRows); // Calculate number of columns
+let playingSoundElement = null;
 
-soundsDiv.style.setProperty('--num-rows', numRows); // Set custom property for number of rows
-soundsDiv.style.setProperty('--num-cols', numCols); // Set custom property for number of columns
+function populateDiv(div, soundsToShow) {
+    soundsToShow.forEach((sound) => {
+        const audioElement = `
+            <audio src="${sound.soundFile}" id="${sound.name}"></audio>
+        `;
 
+        function playThisSound() {
+            playSound(audioElement);
+        };
 
-let playingSound;
+        const soundElement = `
+            <div class="soundButton" onClick="() => {
+                playSound(${playThisSound});
+            }
+                <p class="soundName">${sound.name}</p>
+                ${audioElement}
+            </div>
+        `;
+        div.innerHTML += soundElement;
+    });
+}
 
-
-function getRandomHexColor() {
-    // Generate random values for red, green, and blue components
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-
-    // Convert the decimal values to hexadecimal
-    const redHex = red.toString(16).padStart(2, '0');
-    const greenHex = green.toString(16).padStart(2, '0');
-    const blueHex = blue.toString(16).padStart(2, '0');
-
-    // Concatenate the hexadecimal values to form a color code
-    const hexColor = `#${redHex}${greenHex}${blueHex}`;
-
-    return hexColor;
+function playSound(soundToPlay) {
+    if (playingSoundElement) stopSound(playingSoundElement);
+    playingSoundElement = soundToPlay;
+    playingSoundElement.play();
 }
 
 
-sounds.forEach((sound) => {
-    const soundElement = document.createElement('div');
-    soundElement.classList.add('soundButton');
-    soundElement.innerHTML = sound.name;
+function stopSound(sound) {
+    document.getElementById(sound.name).pause();
+    document.getElementById(sound.name).currentTime = 0;
+}
 
-    soundElement.addEventListener('click', () => {
-        stopSounds();
-        if (playingSound !== sound.name) {
-            console.log(playingSound, sound.name);
-            document.getElementById(sound.name).play();
-            playingSound = sound.name;
-        } else playingSound = null;
-    });
 
-    soundsDiv.appendChild(soundElement);
-    const audioElement = document.createElement('audio');
-    audioElement.src = sound.soundFile;
-    audioElement.id = sound.name;
-
-    soundsDiv.appendChild(audioElement);
+document.addEventListener('DOMContentLoaded', () => {
+    populateDiv(soundsDiv, sounds);
 });
-
-function stopSounds() {
-    sounds.forEach((sound) => {
-        document.getElementById(sound.name).pause();
-        document.getElementById(sound.name).currentTime = 0;
-    });
-}
---
+++ js/sounds.js
@@ -0,0 +1,27 @@
+const sounds = [
+    {
+        name: "John Doe", 
+        soundFile: "sounds/1.wav"
+    },
+    {
+        name: "Jane Doe",
+        soundFile: "sounds/boom.wav"
+    },
+    {
+        name: "Hund",
+        soundFile: "sounds/dog.wav"
+    },
+    {
+        name: "laugh",
+        soundFile: "sounds/laugh.wav"
+    },
+    {
+        name: "retro",
+        soundFile: "sounds/retro.wav"
+    },
+    {
+        name: "oooh my god",
+        soundFile: "sounds/ohmygod.wav",    
+        image: "images/ohmygod.jpg"
+    }
+]
